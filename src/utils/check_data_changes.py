15f0f21f8a73e3193f3c508a3120e934f73f5f1a
Kaito117 <pranshuraj65536@gmail.com>
2024-09-04 19:28:56 +0530

    working util, data files are same

--- src/utils/check_data_changes.py
+++ src/utils/check_data_changes.py
@@ -17,37 +17,36 @@ assert type(locomotion_set) == list, "Yaml not loaded correctly"
 
 def get_file(filepath: str):
     """
-    Gets the data file with required subset columns."""
+    Gets the data file with required subset columns.
+    """
     df = pd.read_csv(filepath, header=None, names=column_names, delimiter=" ")
     return df[locomotion_set]
 
-def get_filenames(data_dir, test_files):
-    os.chdir(data_dir)
-    train_filenames = []
-    test_filenames = []
-
-    for folder in os.listdir():
-        if folder not in test_files:
-            os.chdir(folder)
-            sensor_path = os.path.join(os.getcwd(), f"{folder}_sensors_data.csv")
-            train_filenames.append(sensor_path)
-            os.chdir("..")
-        else:
-            os.chdir(folder)
-            sensor_path = os.path.join(os.getcwd(), f"{folder}_sensors_data.csv")
-            test_filenames.append(sensor_path)
-            os.chdir("..")
-    return train_filenames, test_filenames
+def get_new_filenames(data_dir):
+    """
+    Get filenames from the new dataset directory.
+    """
+    new_filenames = []
+    for root, _, files in os.walk(data_dir):
+        for file in files:
+            if file.endswith('_sensors_data.txt'):
+                new_filenames.append(os.path.join(root, file))
+    return new_filenames
 
 def get_old_filenames(data_dir):
-    filenames = os.listdir(data_dir)
-    fnames = []
-    for filename in filenames:
+    """
+    Get filenames from the old dataset directory.
+    """
+    old_filenames = []
+    for filename in os.listdir(data_dir):
         if filename.endswith('.dat'):
-            fnames.append(os.path.join(data_dir, filename))
-    return fnames
+            old_filenames.append(os.path.join(data_dir, filename))
+    return old_filenames
 
 def compare_files(new_file, old_file):
+    """
+    Compare the contents of the new and old files.
+    """
     new_df = pd.read_csv(new_file, header=None, names=column_names, delimiter=" ")
     old_df = pd.read_csv(old_file, header=None, names=column_names, delimiter=" ")
     
@@ -57,22 +56,24 @@ def compare_files(new_file, old_file):
         diff = new_df.compare(old_df)
         return False, diff
 
-# Load the new and old dataset files
-new_filenames, _ = get_filenames(new_dataset_dir, [])
+new_filenames = get_new_filenames(new_dataset_dir)
 old_filenames = get_old_filenames(old_dataset_dir)
 
 for new_file in new_filenames:
-    new_basename = os.path.basename(new_file).replace(".csv", ".dat")
-    old_file = next((f for f in old_filenames if os.path.basename(f) == new_basename), None)
-    print(old_file)
+    new_basename = os.path.basename(new_file).replace("_sensors_data.txt", ".dat")
+    old_file = None
+    for old in old_filenames:
+        if os.path.basename(old) == new_basename:
+            old_file = old
+            break
+    
     if old_file:
         print(f"Comparing {new_file} with {old_file}")
         are_same, diff = compare_files(new_file, old_file)
         if are_same:
-            print(f"{new_file} and {old_file} are the same.")
+            print("same\n")
         else:
-            print(f"{new_file} and {old_file} are different.")
+            print(f"{new_file} and {old_file} are different.\n")
             print(diff)
     else:
-        pass
-        #print(f"Corresponding old file for {new_file} not found.")
+        print(f"Corresponding old file for {new_file} not found.")
